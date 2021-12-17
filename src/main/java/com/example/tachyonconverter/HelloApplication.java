69e8ad6caa52934da6b4c6ec50eff58a581286e2
Hannan Khan <hannankhan888@gmail.com>
2021-12-16 20:19:05 -0600

    Changed name

--- src/main/java/com/example/tachyonconverter/HelloApplication.java
+++ src/main/java/com/example/tachyonconverter/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Click this button!");
+        stage.setTitle("TachyonConverter");
         stage.setScene(scene);
         stage.show();
     }
